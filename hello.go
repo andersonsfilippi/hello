b4b631a4be1cd7478a23edb6ab8eb49fb18f06b8
Anderson Filippi <andersonfilippi@gmail.com>
2024-06-14 16:17:31 -0300

    teste excecoes

--- hello.go
+++ hello.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"hello/math"
+	"log"
 )
 
 func main() {
-	resultado := math.Soma(1, 2)
-	fmt.Printf("%v\n", resultado)
+	res, err := soma(9, 1)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	fmt.Println(res)
+
+	// o operador _ (underline) pode ser usado para ignorar
+	// completamento um retorno de erro porém não é uma boa prática
+	// pois o idel é tratar o erro
+	res, _ = soma(10, 20)
+	fmt.Println(res)
+}
+
+func soma(x int, y int) (int, error) {
+	res := x + y
+	if res > 10 {
+		return 0, errors.New("total maior que 10")
+	}
+	return res, nil
 }
